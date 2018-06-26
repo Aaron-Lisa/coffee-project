7cc25e1ba101a4f7796f0f93e26eb4a769ccba02
Aaron Trevino <Aaron@Aarons-MacBook-Pro.local>
2018-06-26 15:51:29 -0500

    added all selector

--- main.js
+++ main.js
@@ -13,7 +13,7 @@
 function renderCoffee(coffee) {
     var html = '<div class ="coffee">';
         html += '<div>';
-        html += '<p><span class = "coffee-name">' + coffee.name +'</span>' + "   ";
+        html += '<p><span class = "coffee-name">' + coffee.name +'</span>' + "  ";
         html += '<span class="coffee-roast">' + coffee.roast +'</span>' + '</p>';
         html += '</div>';
         html += '</div>';
@@ -44,29 +44,31 @@ function updateCoffees(e) {
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
     coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
+        if (coffee.roast === selectedRoast || coffee.all === selectedRoast) {
             filteredCoffees.push(coffee);
         }
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
+
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'light', all: "all"},
+    {id: 2, name: 'Half City', roast: 'light', all: "all"},
+    {id: 3, name: 'Cinnamon', roast: 'light', all: "all"},
+    {id: 4, name: 'City', roast: 'medium', all: "all"},
+    {id: 5, name: 'American', roast: 'medium', all: "all"},
+    {id: 6, name: 'Breakfast', roast: 'medium', all: "all"},
+    {id: 7, name: 'High', roast: 'dark', all: "all"},
+    {id: 8, name: 'Continental', roast: 'dark', all: "all"},
+    {id: 9, name: 'New Orleans', roast: 'dark', all: "all"},
+    {id: 10, name: 'European', roast: 'dark', all: "all"},
+    {id: 11, name: 'Espresso', roast: 'dark', all: "all"},
+    {id: 12, name: 'Viennese', roast: 'dark', all: "all"},
+    {id: 13, name: 'Italian', roast: 'dark', all: "all"},
+    {id: 14, name: 'French', roast: 'dark', all: "all"},
+
 ];
 
 
